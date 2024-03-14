d8ed96edf29307e7899b69c3194e4db05857dc9b
Lucca Mancusi <luccaromano@gmail.com>
2024-03-14 10:34:53 -0300

    Delete hero

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -15,7 +15,12 @@ class Database {
     return JSON.parse(file.toString());
   }
   async writeFile(data) {
-    await writeFileAsync(this.NAME_FILE, JSON.stringify(data));
+    try {
+      const result = await writeFileAsync(this.NAME_FILE, JSON.stringify(data));
+      return true;
+    } catch {
+      return false;
+    }
   }
   async register(hero) {
     const data = await this.getFileData();
@@ -31,6 +36,18 @@ class Database {
       return id ? item.id === id : true;
     });
     return filteredData;
+  }
+  async removeHero(id) {
+    if (!id) {
+      return await this.writeFile([]);
+    }
+    const data = await this.getFileData();
+    const index = data.findIndex((item) => item.id === parseInt(id));
+    if (index === -1) {
+      throw Error("Informed hero does not exist!");
+    }
+    data.splice(index, 1);
+    return this.writeFile(data);
   }
 }
 
--- 06-cli/test.js
+++ 06-cli/test.js
@@ -4,6 +4,9 @@ const database = require("./database");
 const DEFAULT_ITEM_REGISTER = { name: "Flash", power: "Speed", id: 1 };
 
 describe("Suite de manipulação de herois", () => {
+  before(async () => {
+    await database.register(DEFAULT_ITEM_REGISTER);
+  });
   it("Should search a hero by id", async () => {
     const expected = DEFAULT_ITEM_REGISTER;
     const [result] = await database.list(expected.id);
@@ -15,5 +18,11 @@ describe("Suite de manipulação de herois", () => {
     const result = await database.register(DEFAULT_ITEM_REGISTER);
     const [actual] = await database.list(DEFAULT_ITEM_REGISTER.id);
     ok(actual, expected);
+  });
+
+  it("Should delete a hero by id", async () => {
+    const expected = true;
+    const result = await database.removeHero(DEFAULT_ITEM_REGISTER.id);
+    deepEqual(result, expected);
   });
 });
